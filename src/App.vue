158825bee67b64988ca45e8ca14152a94c198450
k <kyonc5@gmail.com>
2022-05-22 18:54:49 +0900

    add v-overlay sample

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,37 @@
     <v-main>
       <HelloWorld/>
     </v-main>
+    <v-row
+        align="center"
+        justify="center"
+    >
+      <v-card
+          height="300"
+          width="250"
+      >
+        <v-row justify="center">
+          <v-btn
+              color="success"
+              class="mt-12"
+              @click="overlay = !overlay"
+          >
+            Show Overlay
+          </v-btn>
+
+          <v-overlay
+              :absolute="absolute"
+              v-model="overlay"
+          >
+            <v-btn
+                color="success"
+                @click="overlay = false"
+            >
+              Hide Overlay
+            </v-btn>
+          </v-overlay>
+        </v-row>
+      </v-card>
+    </v-row>
   </v-app>
 </template>
 
@@ -17,7 +48,8 @@ export default {
   },
 
   data: () => ({
-    //
+    absolute: true,
+    overlay: false,
   }),
 }
 </script>
